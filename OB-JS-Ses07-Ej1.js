2fd0eb3dd3884d2373183e5a88b9fbea5eb2dfcb
Dairojhon <112525272+Dairojhon@users.noreply.github.com>
2022-10-21 18:15:25 -0500

    first commit

---
+++ OB-JS-Ses07-Ej1.js
@@ -0,0 +1,7 @@
+const familia= ["Jhon","Claudia", "Diana", "Joe", "Fuhrer", "Carolina"]
+const miFamilia = new Set(familia)
+console.log(miFamilia)
+miFamilia.add("Jhon")
+console.log(miFamilia)
+miFamilia.add("JavaScript")
+console.log(miFamilia)
